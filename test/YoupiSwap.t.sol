152071d76b0506bdc830be48a6e7ad87a673410f
bastien707 <bastien.facqueur@efrei.net>
2023-08-04 11:57:43 +0200

    test: test get constants

---
+++ test/YoupiSwap.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import { Test, console2 } from "forge-std/Test.sol";
+import { YoupiSwap } from "../src/YoupiSwap.sol";
+
+contract CounterTest is Test {
+    YoupiSwap public youpiSwap;
+
+    function setUp() public {
+        youpiSwap = new YoupiSwap();
+    }
+
+    function test_getLinkAddress() public {
+        assertEq(youpiSwap.LINK(), 0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+    }
+
+    function test_getWethAddress() public {
+        assertEq(youpiSwap.WETH(), 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
+    }
+}
